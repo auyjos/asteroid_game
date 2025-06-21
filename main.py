880f861342a0cf5890e57103de9a4fe1000b67ed
auyjos <josea13p@gmail.com>
2025-06-20 22:19:35 -0600

    feat: added circle class and fps

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
-        pygame.Surface.fill(screen, (0, 0, 0))
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
                 return
-   
+
+        screen.fill("black")
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
-    
     main()
-    
